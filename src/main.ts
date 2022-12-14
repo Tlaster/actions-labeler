81828704cae0f0f6fbabd5f1ff7caedd40696f37
Tlaster <tlaster@outlook.com>
2022-12-14 10:37:37 +0000

    init

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,69 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const token: string = core.getInput('github-token')
+    const octokit = github.getOctokit(token)
+    // check if this is a pull request
+    if (github.context.payload.pull_request) {
+      const wipStartWith = core.getInput('wip-start-with')
+      const wipLabel = core.getInput('wip-label')
+      const readyLabel = core.getInput('ready-label')
+      const approvedLabel = core.getInput('approved-label')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+      const pullRequest = github.context.payload.pull_request
+  
+      // check if the pull request title starts with the WIP string
+      if (pullRequest.title.startsWith(wipStartWith)) {
+        // check if label is already set
+        if (!pullRequest.labels.some((label: any) => label.name === wipLabel)) {
+          // add label
+          await octokit.rest.issues.addLabels({
+            ...github.context.repo,
+            issue_number: pullRequest.number,
+            labels: [wipLabel]
+          })
+        }
+      } else {
+        // check if label is set
+        if (pullRequest.labels.some((label: any) => label.name === wipLabel)) {
+          // remove label
+          await octokit.rest.issues.removeLabel({
+            ...github.context.repo,
+            issue_number: pullRequest.number,
+            name: wipLabel
+          })
+        }
+      }
 
-    core.setOutput('time', new Date().toTimeString())
+      // check if the pull request is not wip and does not have the ready label
+      if (!pullRequest.title.startsWith(wipStartWith) && !pullRequest.labels.some((label: any) => label.name === readyLabel)) {
+        // check if the pull request has the approved label
+        if (pullRequest.labels.some((label: any) => label.name === approvedLabel)) {
+          // add label
+          await octokit.rest.issues.addLabels({
+            ...github.context.repo,
+            issue_number: pullRequest.number,
+            labels: [readyLabel]
+          })
+        }
+      }
+
+      // check if the pull request is all approved and does not have the approved label
+      if (pullRequest.labels.some((label: any) => label.name === readyLabel) && !pullRequest.labels.some((label: any) => label.name === approvedLabel)) {
+        // check if the pull request has the approved label
+        if (pullRequest.requested_reviewers.length === pullRequest.requested_reviewers.filter((reviewer: any) => reviewer.state === 'APPROVED').length) {
+          // add label
+          await octokit.rest.issues.addLabels({
+            ...github.context.repo,
+            issue_number: pullRequest.number,
+            labels: [approvedLabel]
+          })
+        }
+      }
+  
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
